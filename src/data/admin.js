4bb2ed88f6f302a8b1d123f6a4342992ac8746c4
Abaya Dev <dev@abaya.com>
2026-02-21 00:11:15 +0300

    إضافة ملف خدمة Firebase وتحديث صلاحيات الأدمن

--- src/data/admin.js
+++ src/data/admin.js
@@ -1,11 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./abaya-marketplace-firebase-adminsdk-fbsvc-206eb1b7a1.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-// ضع هنا UID الخاص بك
+// ضع UID الخاص بك هنا
 const uid = "SS6Xip7MDrVAmiWUNMVyebprbi53";
 
 admin.auth().setCustomUserClaims(uid, { admin: true })
